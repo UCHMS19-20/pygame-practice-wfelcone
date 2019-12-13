34c97167fa9e45714d954c6788c53b4e05ff4760
jgerstein <jgerstein@ucvts.tec.nj.us>
2019-12-13 09:44:23 -0500

    Initialize 34b

---
+++ minimal.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+# Initialize pygame so it runs in the background and manages things
+pygame.init()
+
+# Colors
+red = pygame.Color(255, 0, 0)
+orange = pygame.Color(255, 128, 0)
+yellow = pygame.Color(255, 255, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+purple = pygame.Color(255, 0, 255)
+
+# Create a display. Size must be a tuple, which is why it's in parentheses
+screen = pygame.display.set_mode( (400, 300) )
+
+# Main loop. Your game would go inside this loop
+while True:
+    # do something for each event in the event queue (list of things that happen)
+    for event in pygame.event.get():
+        print(event)
+        # Check to see if the current event is a QUIT event
+        if event.type == pygame.QUIT:
+            # If so, exit the program
+            sys.exit()
